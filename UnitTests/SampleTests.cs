91398984aa312ae15f0888996566e2f6f239b363
Igor Kliushnikov <igor.kliushnikov@hotmail.com>
2018-08-24 14:39:33 +0200

    Unit tests project with NUnit and FluentAssertion

---
+++ UnitTests/SampleTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class SampleTests
+    {
+        // NUnit. take a look on "writing tests" clause in the wiki: https://github.com/nunit/docs/wiki/NUnit-Documentation
+        // Fluent samples: https://fluentassertions.com/examples/
+
+        [Test]
+        public void Succeed()
+        {
+            true.Should().Be(true, "happy path");
+        }
+
+        [Test]
+        public void Failed()
+        {
+            false.Should().Be(true, "failure");
+        }
+    }
+}
